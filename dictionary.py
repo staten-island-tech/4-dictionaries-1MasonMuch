762376e48b11e6acebfec0fc2556d02bd6005406
1MasonMuch <masonm126@nycstudents.net>
2025-10-08 06:25:27 -0700

    8

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,40 @@
-best_buy_items = [
+mason_store = [ {
+
 {
-     "name": "Samsung 55\" 4k UHD TV",
-     "price": 429.99, 
-     "deoartment": "Televisions", 
-     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-} 
-    ]
-print(best_buy_items[0]["name"])
+    "name": "RTX 5090",
+    "price": 4000,
+    "rating": "High End",
+    "description": "32 GB of GDDR7 Memory and currently the latest of Nvidias 50 series graphics cards."
+},
+
+
+ 
+   { "name": "Apple Airpods Max",
+    "price": 500, 
+    "rating": "Mid End",
+    "description": "Headphones with great noise cancellation features and compatible with all of Apples Products."
+},
+{
+    "name": "Iphone 17 pro",
+    "price": 1000,
+    "rating": "High end",
+    "description": "Apple's latest Iphone with the newest Apple 19 chip and a 48 megapixel camera"
+
+},
+{
+    "name": "Xiyang phone",
+    "price": 1000000,
+    "rating": "High End",
+    "description": "A phone made of 24 karat gold with Xiyangs face on it"
+},
+{
+    "name": "Mr. Whalen Key Board",
+    "price": 1,
+    "rating": "Low End",
+    "description": "A keyboard with MR. Whalen's face on every key cap. Once every 100 presses th keyboard will talk about the world's issues."
+}
+
+}
+
+]
+
