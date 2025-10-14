0e618ecbdd52acf2772cb8b382c082c95ceaccbb
1MasonMuch <masonm126@nycstudents.net>
2025-10-14 06:26:28 -0700

    67

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,4 @@
-mason_store = [ {
+mason_store = [ 
 
 {
     "name": "RTX 5090",
@@ -34,7 +34,31 @@ mason_store = [ {
     "description": "A keyboard with MR. Whalen's face on every key cap. Once every 100 presses th keyboard will talk about the world's issues."
 }
 
-}
-
 ]
 
+for index, item in enumerate(mason_store):
+    print(index, ':', item['name'])
+
+cart = input("Input your cart like this: 0, 1, 2, 3, 4 You CAN repeat numbers.")
+rtx = 0
+max = 0
+phone = 0
+xiyang = 0
+whalen = 0
+total = 0
+for char in cart:
+    if char == "0":
+        rtx = rtx + 1
+        total += + 4000
+    elif char == "1":
+        max = max + 1
+        total += 500
+    elif char == "2":
+        phone = phone + 1
+        total += 1000
+    elif char == "3":
+        xiyang = xiyang + 1
+        total += 1000000
+    elif char == "4":
+        whalen = whalen +1
+        total += 1
