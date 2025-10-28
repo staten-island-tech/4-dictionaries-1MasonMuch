e1fd922f1dc86464c4105c39c006e249e837f215
1MasonMuch <masonm126@nycstudents.net>
2025-10-28 06:11:46 -0700

    I AM DONE

--- dictionary.py
+++ dictionary.py
@@ -1,61 +1,78 @@
 mason_store = [ 
 
 { 
+
+    "itemnumber": "0", 
     "name": "RTX 5090",
     "price": 4000,
     "rating": "High End",
-    "description": "32 GB of GDDR7 Memory and currently the latest of Nvidias 50 series graphics cards."
-
+    "description": "32 GB of GDDR7 Memory and currently the latest of Nvidias 50 series graphics cards.",
+    "amount": 0
+    
 },
 
 
  
-   { "name": "Apple Airpods Max",
+ {   
+    "itemnumber": "1",
+    "name": "Apple Airpods Max",
     "price": 500, 
     "rating": "Mid End",
-    "description": "Headphones with great noise cancellation features and compatible with all of Apples Products."
+    "description": "Headphones with great noise cancellation features and compatible with all of Apples Products.",
+    "amount": 0
+
 },
-{  
+{   
+    "itemnumber": "2",
     "name": "Iphone 17 pro",
     "price": 1000,
     "rating": "High end",
-    "description": "Apple's latest Iphone with the newest Apple 19 chip and a 48 megapixel camera"
-
+    "description": "Apple's latest Iphone with the newest Apple 19 chip and a 48 megapixel camera",
+    "amount": 0
 },
-{
+{   
+    "itemnumber": "3",
     "name": "Xiyang phone",
     "price": 1000000,
     "rating": "High End",
-    "description": "A phone made of 24 karat gold with Xiyangs face on it"
+    "description": "A phone made of 24 karat gold with Xiyangs face on it",
+    "amount": 0
 },
-{
+{   
+    "itemnumber": "4",
     "name": "Mr. Whalen Key Board",
     "price": 1,
     "rating": "Low End",
-    "description": "A keyboard with MR. Whalen's face on every key cap. Once every 100 presses this keyboard will lower your HOS."
+    "description": "A keyboard with MR. Whalen's face on every key cap. Once every 100 presses this keyboard will lower your HOS.",
+    "amount": 0
 }
 
 ]
+ 
+total = 0
 
-rtx = 0
-max = 0
-phone = 0
-xiyang = 0 
-whalen = 0
-total = 0 
 cart = ""
 
 
 for index, item in enumerate(mason_store):
-    print(index, ':', item['name'], item['price'], item ['description'], item['rating'])
+    print(index, ':',item['itemnumber'], item['name'], item['price'], item ['description'], item['rating'])
     
-    
+
 while cart != "done" :
     cart = input("Input what you want. type 'done' when done")
-    for i in mason_store:
-        if cart.lower() == mason_store.lower():
+    
+    for item in mason_store:
+        if cart == item["itemnumber"]:
+            item["amount"] = item["amount"] + 1
+            
 
+for item in mason_store:
+    if item['amount'] > 0:
+        total = (item['amount'] * item['price']) + total 
 
-print(f"You have orderd {rtx} RTX 5090(s), {max} Apple Airpods Max(s), {phone} Iphone 17 pro(s), {xiyang} Xiyang phone(s), and {whalen} Mr. Whalen Key Board(s)")
-print("__________________________________________________________________________________________________________________________________________________________________")
-print(f"your cart total is {total} dollas.")
+if cart == "done":
+    for item in mason_store:
+        if item['amount'] > 0:
+            print(f" You have {item['name']} {item['amount']}")
+            
+print(f" Your total is {total}")
---
+++ test.py
@@ -0,0 +1,32 @@
+
+
+def broke(q,a,b,c):
+    total = 0
+    
+    while q > 0:
+            total = total +1
+            q = q - 1
+            a = a + 1 
+            if a == 35:
+                a = 0
+                q = q + 30
+            
+            if q > 0:
+                total = total + 1 
+                q = q - 1
+                b = b + 1 
+                if b == 100:
+                    b = 0
+                    q = q + 60
+            if q > 0:
+                total = total + 1       
+                q = q - 1
+                c = c + 1 
+                if c == 10:
+                    c = 0
+                    q = q + 9 
+         
+    print(f" {q} , {a} , {b} , {c} , {total}")
+    
+    print(f"Martha plays {total} times before going broke")
+broke(77,4,9,3)
